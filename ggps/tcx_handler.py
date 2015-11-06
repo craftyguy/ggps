4220469b59ef8e1095d21d95148ae266a5ed764b
Chris Joakim <christopher.joakim@gmail.com>
2015-11-06 05:47:39 -0500

    Added dav_track_5k data files, and runcadencex2 augmentation.

--- ggps/tcx_handler.py
+++ ggps/tcx_handler.py
@@ -27,6 +27,9 @@ class TcxHandler(xml.sax.ContentHandler):
         self.curr_tkpt = Trackpoint()
         self.current_text = ''
         self.end_reached = False
+        self.first_time = None
+        self.first_etime = None
+        self.first_time_secs_to_midnight = 0
 
     def startElement(self, tag_name, attrs):
         self.heirarchy.append(tag_name)
@@ -81,7 +84,13 @@ class TcxHandler(xml.sax.ContentHandler):
 
         if self.augment:
             for idx, t in enumerate(self.trackpoints):
+                if idx == 0:
+                    self.set_first_trackpoint(t)
                 self.augment_with_calculations(idx, t)
+
+    def set_first_trackpoint(self, t):
+        self.first_time = self.parse_hhmmss(t.get('time'))
+        self.first_etime = m26.ElapsedTime(self.first_etime)
 
     def reset_curr_text(self):
         self.current_text = ''
@@ -102,6 +111,10 @@ class TcxHandler(xml.sax.ContentHandler):
         t.set('seq', "{0}".format(idx + 1))
         self.meters_to_feet(t, 'altitudemeters', 'altitudefeet')
         self.meters_to_miles(t, 'distancemeters', 'distancemiles')
+        self.meters_to_km(t, 'distancemeters', 'distancekilometers')
+        self.cadence_x2(t)
+
+        self.calculate_elapsed_time(t)
 
     def meters_to_feet(self, t, meters_key, new_key):
         m = t.get(meters_key)
@@ -111,6 +124,12 @@ class TcxHandler(xml.sax.ContentHandler):
             yds = d_km.as_yards()
             t.set(new_key, str(yds * 3.000000))
 
+    def meters_to_km(self, t, meters_key, new_key):
+        m = t.get(meters_key)
+        if m:
+            km = float(m) / 1000.0
+            t.set(new_key, str(km))
+
     def meters_to_miles(self, t, meters_key, new_key):
         m = t.get(meters_key)
         if m:
@@ -118,6 +137,35 @@ class TcxHandler(xml.sax.ContentHandler):
             d_km = m26.Distance(km, m26.Constants.uom_kilometers())
             t.set(new_key, str(d_km.as_miles()))
 
+    def meters_to_miles(self, t, meters_key, new_key):
+        m = t.get(meters_key)
+        if m:
+            km = float(m) / 1000.0
+            d_km = m26.Distance(km, m26.Constants.uom_kilometers())
+            t.set(new_key, str(d_km.as_miles()))
+
+    def cadence_x2(self, t):
+        c = t.get('runcadence')
+        if c:
+            i = int(c)
+            t.set('runcadencex2', str(i * 2))
+
+    def calculate_elapsed_time(self, t):
+        new_key = 'elapsedtime'
+        time_str = t.get('time')
+        if time_str == self.first_time:
+            t.set(new_key, '00:00:00')
+        else:
+            t.set(new_key, self.parse_hhmmss(time_str))
+
+    def parse_hhmmss(self, time_str):
+        """
+        For a given value like '2014-10-05T17:22:17.000Z' return the hhmmss '17:22:17' part.
+        """
+        if len(time_str) == 24:
+            return time_str.split('T')[1][:9]
+        else:
+            return ''
 
 if __name__ == "__main__":
     filename = sys.argv[1]
