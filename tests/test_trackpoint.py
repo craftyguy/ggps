2f69a2c336f850ccb72c2bbfb62d6cfc8a6c92d7
Chris Joakim <christopher.joakim@gmail.com>
2017-09-27 06:32:44 -0400

    Converted tests/test_trackpoint.py to pytest

--- tests/test_trackpoint.py
+++ tests/test_trackpoint.py
@@ -1,42 +1,26 @@
 
 import json
-import unittest
+import pytest
 
 import ggps
 
 
-class TrackpointTest(unittest.TestCase):
+def test_str():
+    t = ggps.Trackpoint()
+    assert(str(t) == '<Trackpoint values count:1>')
 
-    def setUp(self):
-        pass
+def test_repr():
+    t = ggps.Trackpoint()
+    t.set('lang', 'python')
+    t.set(None, 'python')
+    j = repr(t)
+    obj = json.loads(j)
+    assert(obj['type'] == 'Trackpoint')
+    assert(obj['lang'] == 'python')
 
-    def tearDown(self):
-        pass
-
-    def test_str(self):
-        t = ggps.Trackpoint()
-        actual = str(t)
-        expected = '<Trackpoint values count:1>'
-        msg = "Should be {0}, got {1}".format(expected, actual)
-        self.assertTrue(actual == expected, msg)
-
-    def test_repr(self):
-        t = ggps.Trackpoint()
-        t.set('lang', 'python')
-        t.set(None, 'python')
-        j = repr(t)
-        obj = json.loads(j)
-        t = obj['type']
-        l = obj['lang']
-        self.assertTrue(t == 'Trackpoint', "expected the value 'Trackpoint'")
-        self.assertTrue(l == 'python', "expected the value 'python'")
-
-    def test_get(self):
-        t = ggps.Trackpoint()
-        t.set('lang', 'python')
-        l = t.get('lang')
-        x = t.get('xxx')
-        z = t.get('zzz', 'zero')
-        self.assertTrue(l == 'python', "expected the value 'python'")
-        self.assertTrue(x == '', "expected the value ''")
-        self.assertTrue(z == 'zero', "expected the value 'zero'")
+def test_get():
+    t = ggps.Trackpoint()
+    t.set('lang', 'python')
+    assert(t.get('lang') == 'python')
+    assert(t.get('xxx') == '')
+    assert(t.get('zzz', 'zero') == 'zero')
