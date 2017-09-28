eb764f77e85e08ba1aaea8ddab668b7ec3edf416
Chris Joakim <christopher.joakim@gmail.com>
2017-09-28 05:26:38 -0400

    Removed comment in test.sh

--- test.sh
+++ test.sh
@@ -23,7 +23,3 @@ echo 'done'
 # python setup.py sdist upload -r pypilegacy
 # ls -al /Users/cjoakim/pypi-packages
 # rm /Users/cjoakim/pypi-packages/ggps*
-
-# Submitting dist/ggps-0.2.0.tar.gz to https://pypi.python.org/pypi
-# Upload failed (410): Gone (This API has been deprecated and removed from legacy PyPI in favor of using the APIs available in the new PyPI.org implementation of PyPI (located at https://pypi.org/). For more information about migrating your use of this API to PyPI.org, please see https://packaging.python.org/guides/migrating-to-pypi-org/#uploading. For more information about the sunsetting of this API, please see https://mail.python.org/pipermail/distutils-sig/2017-June/030766.html)
-# error: Upload failed (410): Gone (This API has been deprecated and removed from legacy PyPI in favor of using the APIs available in the new PyPI.org implementation of PyPI (located at https://pypi.org/). For more information about migrating your use of this API to PyPI.org, please see https://packaging.python.org/guides/migrating-to-pypi-org/#uploading. For more information about the sunsetting of this API, please see https://mail.python.org/pipermail/distutils-sig/2017-June/030766.html)
