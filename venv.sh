09c1e5823e2a4866635f9227290822c3264aec4c
Chris Joakim <christopher.joakim@gmail.com>
2020-02-22 07:27:40 -0500

    enhanced venv.sh for create, delete, etc

--- venv.sh
+++ venv.sh
@@ -1,29 +1,82 @@
 #!/bin/bash
 
-# Recreate the virtual environment and reinstall libs.
-# Requires Python 3; version 3.6 recommended.
-# Chris Joakim, 2017/09/28
+# Bash shell script to delete or create the Python Virtual Environment
+# used by this library.  Requires Python 3; version 3.7+ is recommended.
+# Chris Joakim, 2020/02/22
 
-echo 'deleting previous venv...'
-rm -rf bin/
-rm -rf lib/
-rm -rf include/
-rm -rf man/
+display_help() {
+    echo "script options:"
+    echo "  ./venv.sh delete"
+    echo "  ./venv.sh create"
+    echo "  ./venv.sh pip_list"
+    echo "  ./venv.sh help"
+}
 
-echo 'creating new venv ...'
-python3 -m venv .
-source bin/activate
-python --version
+delete_venv() {
+    echo 'deleting previous venv...'
+    rm -rf bin/
+    rm -rf include/
+    rm -rf lib/
+    rm -rf man/
+    echo 'done'
+}
 
-echo 'upgrading pip-tools ...'
-pip install --upgrade pip-tools
+create_venv() {
+    echo 'creating new venv ...'
+    python3 -m venv .
+    source bin/activate
+    python --version
+    pip --version
 
-echo 'pip-compile requirements.in ...'
-pip-compile --output-file requirements.txt requirements.in
+    echo 'installing/upgrading pip...'
+    pip install --upgrade pip
+    pip --version
 
-echo 'pip install requirements.txt ...'
-pip install -r requirements.txt
+    echo 'installing/upgrading pip-tools...'
+    pip install --upgrade pip-tools
 
-pip list --format=columns
+    echo 'pip-compile requirements.in ...'
+    pip-compile --output-file requirements.txt requirements.in
 
-echo 'done'
+    echo 'pip install requirements.txt ...'
+    pip install -r requirements.txt
+
+    pip list --format=columns
+
+    echo 'next: source bin/activate ; python --version'
+    echo 'done'
+}
+
+pip_list() {
+    echo 'pip_list ...'
+    pip list --format=columns
+    echo 'done'
+}
+
+arg_count=$#
+
+if [ $arg_count -gt 0 ]
+then
+    if [ $1 == "help" ] 
+    then
+        display_help
+    fi
+
+    if [ $1 == "delete" ] 
+    then
+        delete_venv
+    fi
+
+    if [ $1 == "create" ] 
+    then
+        delete_venv
+        create_venv
+    fi
+
+    if [ $1 == "pip_list" ] 
+    then
+        pip_list
+    fi
+else
+    display_help
+fi
